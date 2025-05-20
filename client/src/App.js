46f2e5adbf2eb4c478062f182f9407ca529e1954
KAIpany <tool2@kaipany.com>
2025-05-20 23:25:39 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -468,7 +468,7 @@ const App = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {resultsData.map((resultsData, index) => (
+                  {resultsData.map((result, index) => (
                     <tr key={result.personID + "_" + index}>
                       <td>{result.personName || "(Không tên)"}</td>
                       <td className="monospace">{result.personID}</td>
