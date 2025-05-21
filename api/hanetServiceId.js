f16b5978f0fd08033ec01555801db740a57459ed
KAIpany <tool2@kaipany.com>
2025-05-21 13:46:14 +0700

    Update hanetServiceId.js

--- api/hanetServiceId.js
+++ api/hanetServiceId.js
@@ -72,7 +72,7 @@ function filterCheckinsByDay(data) {
 }
 
 function formatTimestamp(timestamp) {
-  const date = new Date(timestamp);
+  const date = new Date(timestamp * 60 * 60 * 7);
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
